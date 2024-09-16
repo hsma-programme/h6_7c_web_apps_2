c2b0480418bd2b8ad193a8685f77fd8b2d67b3cd
Sammi Rosser <samanthajaderosser@gmail.com>
2024-09-16 14:07:22 +0100

    Update more complex examples

--- code_examples/multipage_app/app.py
+++ code_examples/multipage_app/app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-pg = st.navigation(
-    [st.Page("home_page.py", title="Welcome!", icon=":material/add_circle:"),
-     st.Page("des_page.py", title="Run Simulation", icon=":material/laptop:"),
-     ]
-     )
+pg = st.navigation([
+        st.Page("home_page.py", title="Welcome!", icon=":material/add_circle:"),
+        st.Page("des_page.py", title="Run Simulation", icon=":material/laptop:")
+     ])
 
 pg.run()
